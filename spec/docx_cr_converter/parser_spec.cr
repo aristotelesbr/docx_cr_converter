48e29fd3338ce7de18b38c4f6662fc6533fb8fd6
Aristoteles <aristotelesbr2014@gmail.com>
2020-03-09 23:04:14 -0300

    added parse method xml

--- spec/docx_cr_converter/parser_spec.cr
+++ spec/docx_cr_converter/parser_spec.cr
@@ -9,6 +9,24 @@ Spectator.describe DocxCrConverter::Parser do
         it { expect(docx.xml).to be_truthy }
         it { expect(docx.xml).to respond_to(:errors?) }
         it { expect(docx.errors?).not_to be_truthy }
+        it { expect(docx.xml.xml_document.is_a?(XML::Node | String)).to be_truthy }
+      end
+    end
+
+    context "#parser" do
+      given path = "./spec/fixtures/file-sample.docx" do
+        it { expect(docx).to be_truthy }
+        it { expect(docx.parse.is_a?(String)).to be_truthy }
+        it { expect(docx.parse).to match(/Lorem ipsum/) }
+      end
+    end
+  end
+
+  describe "When invalid data" do
+    context "with invalid docx" do
+      given path = "./spec/fixtures/file-sample.docx" do
+        it "with errors messages" do
+        end
       end
     end
   end
--- src/docx_cr_converter/parser.cr
+++ src/docx_cr_converter/parser.cr
@@ -11,5 +11,52 @@ module DocxCrConverter
     def errors?
       @errors.size > 0
     end
+
+    def parse
+      return @errors << "Invalid XML::Node parser" unless @xml.xml_document.is_a?(XML::Node)
+
+      output = ""
+      @xml.xml_document.as(XML::Node).xpath_nodes("//*[name()='w:p']").map do |child|
+        case child.name
+        when "p"
+          output += "\n\n"
+          child.children.map do |r|
+            case r.name
+            when "pPr"
+              # Get Styles
+              r.children.map do |s|
+                case s.name
+                when "pStyle"
+                  case s["val"]
+                  when "Title"
+                    output += "# \n"
+                  when "Heading1"
+                    output += "# "
+                  when "Heading2"
+                    output += "## "
+                  when "Quote"
+                    output += "> "
+                  when "Normal.0"
+                    output += "\n\n"
+                  end
+                when "numPr"
+                  output += "+ "
+                when "rPr"
+                  # Get Bold style
+                end
+              end
+            else
+              # Get text
+              r.children.map do |x|
+                case x.name
+                when "t"
+                  return (output += x.text).as(String)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
