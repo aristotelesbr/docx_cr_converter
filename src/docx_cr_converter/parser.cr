4f2941b51e9ae039aafe74acfcd0dea60e86bf13
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2020-11-01 22:19:47 +0000

    Fix TooManyWords
    This version allows you to select only those words that need to be changed. If you care about performance, be careful. No refactoring.

--- src/docx_cr_converter/parser.cr
+++ src/docx_cr_converter/parser.cr
@@ -6,10 +6,6 @@ module DocxCrConverter
       @xml = DocxCrConverter::ExtractFiles.new(@docx_path)
       @errors = [] of String
       @document = ""
-      @bold = 0
-      @italics = 0
-      @strike = 0
-      @insert = 0
     end
 
     def errors?
@@ -19,101 +15,87 @@ module DocxCrConverter
     def parse
       return @errors << "Invalid XML::Node parser" unless @xml.xml_document.is_a?(XML::Node)
 
-      @xml.xml_document.as(XML::Node).xpath_nodes("//*[name()='w:p']").map do |child|
-        case child.name
-        when "p"
-          @document += "\n\n"
-          child.children.map do |r|
-            case r.name
-            when "pPr"
-              # Get Styles
-              r.children.map do |s|
-                case s.name
-                when "pStyle"
-                  case s["val"]
-                  when "Title"
-                    @document += "# \n"
-                  when "Heading1"
-                    @document += "# "
-                  when "Heading2"
-                    @document += "## "
-                  when "Heading3"
-                    @document += "### "
-                  when "Heading4"
-                    @document += "#### "
-                  when "Heading5"
-                    @document += "##### "
-                  when "Quote"
-                    @document += "> "
-                  when "Normal.0"
-                    @document += "\n\n"
-                  when "TextBody"
-                    if r.to_s.includes?("iCs")
-                      # || r.to_s.includes?("i")
-                      @italics = 1
-                      if @italics == 1
-                        @document += "*"
-                        @italics == 0
-                      end
-                    end
+      @xml.xml_document.as(XML::Node).xpath_nodes("w:document/w:body/w:p/w:pPr").each do |x|
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Title")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " # #{c} <br />"
+        end
 
-                    if r.to_s.includes?("bCs")
-                      # || r.to_s.includes?("b")
-                      @bold = 1
-                      if @bold == 1
-                        @document += "**"
-                        @bold == 0
-                      end
-                    end
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Subtitle")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " ## #{c} <br />"
+        end
 
-                    if r.to_s.includes?("dstrike")
-                      # || r.to_s.includes?("strike")
-                      @strike = 1
-                      if r.to_s.includes?("false")
-                        @strike = 2
-                      end
-                      if @strike == 1
-                        @document += "<del>"
-                        @strike == 0
-                      end
-                    end
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Heading1")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " # #{c} <br />"
+        end
+
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Heading2")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " ## #{c} <br />"
+        end
+
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Heading3")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " ### #{c} <br />"
+        end
+
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Heading4")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " #### #{c} <br />"
+        end
+
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Heading5")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " ##### #{c} <br />"
+        end
+
+        if x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?("Heading6")
+          a = x.parent
+          b = a.as(XML::Node).xpath_nodes("w:r/w:t")
+          c = b.to_s[5..-1].sub("</w:t>", "")
+          @document += " ###### #{c} <br />"
+        end
+
+        n = x.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_a
+        n.each do |g|
+          if g.to_s.includes?("TextBody")
+            n = g.parent
+            b = n.as(XML::Node).parent
+            c = b.as(XML::Node).parent
+
+            if c.as(XML::Node).xpath_nodes("w:r/w:rPr/w:bCs")
+              unless c.as(XML::Node).xpath_nodes("w:r/w:rPr/w:bCs").to_s.includes?("false")
+                if c.to_s.includes?("bCs")
+                  d = c.as(XML::Node).xpath_nodes("w:r/w:t").to_s.sub("</w:t>", "").sub("<w:t>", "")
+                  unless d.empty?
+                    @document += " **#{d}** <br />"
                   end
-                when "numPr"
-                  @document += "+ "
-
-                  if r.to_s.includes?("iCs") &&
-                     r.to_s.includes?("false")
-                    @italics = 2
-                  end
-
-                  if r.to_s.includes?("bCs") &&
-                     r.to_s.includes?("false")
-                    @bold = 2
-                  end
-                when "u"
-                  @insert == 1
-                  @document += "<ins>"
-                  @insert == 0
                 end
               end
-            else
-              # Get text
-              r.children.map do |x|
-                case x.name
-                when "t"
-                  @document += x.text
+            end
 
-                  if @bold == 1
-                    @document += "**"
-                  end
-                  if @italics == 1
-                    @document += "*"
-                  end
-                  if @strike == 1
-                    @document += "</del>"
-                  end
-                  if @insert == 1
-                    @document += "</ins>"
+            if c.as(XML::Node).xpath_nodes("w:r/w:rPr/w:iCs")
+              unless c.as(XML::Node).xpath_nodes("w:r/w:rPr/w:iCs").to_s.includes?("false")
+                if c.to_s.includes?("iCs")
+                  d = c.as(XML::Node).xpath_nodes("w:r/w:t").to_s.sub("</w:t>", "").sub("<w:t>", "")
+                  unless d.empty?
+                    @document += " *#{d}* <br />"
                   end
                 end
               end
