fa1fcd8a1c09d86445eb41a747970b3ec381cb64
Rafael Pszenny <34192273+onliniak@users.noreply.github.com>
2020-11-03 19:29:20 +0000

    Indentation

--- src/docx_cr_converter/macros.cr
+++ src/docx_cr_converter/macros.cr
@@ -1,27 +1,26 @@
 macro heading(header, document)
   {% begin %}
-    if node.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?({{header}})
-          main_node = node.parent.as(XML::Node).xpath_nodes("w:r/w:t")
-          word = main_node.to_s[5 .. -7]
-          @document += {{document}}
-        end
+  if node.as(XML::Node).xpath_nodes("w:pStyle/@w:val").to_s.includes?({{header}})
+    main_node = node.parent.as(XML::Node).xpath_nodes("w:r/w:t")
+    word = main_node.to_s[5 .. -7]
+    @document += {{document}}
+  end
   {% end %}
 end
 
 macro text_style(style, document)
   {% begin %}
-      style = {{style}}
-     if get_text.as(XML::Node).xpath_nodes("w:r/w:rPr/w:#{style}")
-              unless get_text.as(XML::Node).xpath_nodes("w:r/w:rPr/w:#{style}").to_s.includes?("false") ||
-                     get_text.as(XML::Node).xpath_nodes("w:r/w:rPr/w:#{style}").to_s.includes?("none")
-                if get_text.to_s.includes?("#{style}")
-                  word = get_text.as(XML::Node).xpath_nodes("w:r/w:t")
-                  unless word.to_s.empty?
-                    @document += {{document}}.sub("<w:t>", "").sub("</w:t>", "")
-                     @changed = true
-                  end
-                end
-              end
-            end
+  style = {{style}}
+  if get_text.as(XML::Node).xpath_nodes("w:r/w:rPr/w:#{style}")
+    unless get_text.as(XML::Node).xpath_nodes("w:r/w:rPr/w:#{style}").to_s.includes("false") || get_text.as(XML::Node).xpath_nodes("w:r/w:rPr/w:#{style}").to_s.includes?("none")
+      if get_text.to_s.includes?("#{style}")
+        word = get_text.as(XML::Node).xpath_nodes("w:r/w:t")
+        unless word.to_s.empty?
+          @document += {{document}}.sub("<w:t>", "").sub("</w:t>", "")
+          @changed = true
+        end
+      end
+    end
+  end
   {% end %}
 end
